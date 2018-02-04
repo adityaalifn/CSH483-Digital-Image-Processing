b58bcddb64dbbe7292d250381a3c3d14cd470723
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-02-04 11:19:54 +0700

    task 1 finished

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route("/", methods = ["post", "get"])
+
+@app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", file_path="img/image_here_123123123.jpg")
 
 
-if __name__ == "__main__":
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, "static/img")
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template("home.html", file_path="img/"+filename)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
