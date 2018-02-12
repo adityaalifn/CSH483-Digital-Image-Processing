bda76b2bd19bd421f18bd79b94bd5a337f9f97b7
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-02-13 00:05:08 +0700

    best checkpoint

--- app.py
+++ app.py
@@ -2,16 +2,28 @@ import numpy as np
 from PIL import Image
 import os
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
 @app.route("/index")
 @app.route("/")
 def index():
     return render_template("home.html", file_path="img/image_here.jpg")
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 @app.route("/about")
