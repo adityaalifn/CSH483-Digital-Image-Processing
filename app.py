4766f893f6fcd6dbc19f8a7bf53528948a0a4c3b
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-02-14 11:36:34 +0700

    add zoomout function

--- app.py
+++ app.py
@@ -143,11 +143,13 @@ def crop():
     middle_y_start = middle_y * 1 // 4
     middle_y_end = middle_y * 3 // 4
 
-    img_arr = img_arr[middle_x_start:middle_x_end, middle_y_start:middle_y_end, :]
+    img_arr = img_arr[middle_x_start:middle_x_end,
+                      middle_y_start:middle_y_end, :]
     img_new = Image.fromarray(img_arr)
     img_new = img_new.convert("RGB")
     img_new.save("static/img/temp_img_zoomin.jpeg")
     return render_template("uploaded.html", file_path="img/temp_img_zoomin.jpeg")
+
 
 @app.route("/zoomin", methods=["POST"])
 @nocache
@@ -156,8 +158,9 @@ def zoomin():
     img = img.convert("RGB")
 
     img_arr = np.asarray(img)
-    #img_arr.setflags(write=1)
-    new_size = ((img_arr.shape[0]//2), (img_arr.shape[1]//2), img_arr.shape[2])
+    # img_arr.setflags(write=1)
+    new_size = ((img_arr.shape[0] // 2),
+                (img_arr.shape[1] // 2), img_arr.shape[2])
     new_arr = np.full(new_size, 255)
     print(img_arr.shape, new_size)
     new_arr.setflags(write=1)
@@ -167,9 +170,12 @@ def zoomin():
     for row in range(img_arr_shape[0]):
         for col in range(img_arr_shape[1]):
             try:
-                new_arr[row,col,0] = (int(img_arr[row,col,0]) + int(img_arr[row+1,col,0]) + int(img_arr[row,col+1,0]) + int(img_arr[row+1,col+1,0])) // 4
-                new_arr[row,col,1] = (int(img_arr[row,col,1]) + int(img_arr[row+1,col,1]) + int(img_arr[row,col+1,1]) + int(img_arr[row+1,col+1,1])) // 4
-                new_arr[row,col,2] = (int(img_arr[row,col,2]) + int(img_arr[row+1,col,2]) + int(img_arr[row,col+1,2]) + int(img_arr[row+1,col+1,2])) // 4
+                new_arr[row, col, 0] = (int(img_arr[row, col, 0]) + int(img_arr[row + 1, col, 0]) + int(
+                    img_arr[row, col + 1, 0]) + int(img_arr[row + 1, col + 1, 0])) // 4
+                new_arr[row, col, 1] = (int(img_arr[row, col, 1]) + int(img_arr[row + 1, col, 1]) + int(
+                    img_arr[row, col + 1, 1]) + int(img_arr[row + 1, col + 1, 1])) // 4
+                new_arr[row, col, 2] = (int(img_arr[row, col, 2]) + int(img_arr[row + 1, col, 2]) + int(
+                    img_arr[row, col + 1, 2]) + int(img_arr[row + 1, col + 1, 2])) // 4
             except:
                 break
             col += 1
@@ -180,10 +186,76 @@ def zoomin():
     img_new.save("static/img/temp_img_zoomin.jpeg")
     return render_template("uploaded.html", file_path="img/temp_img_zoomin.jpeg")
 
+
 @app.route("/zoomout", methods=["POST"])
 @nocache
 def zoomout():
-    return render_template("uploaded.html", file_path="img/temp_img.jpeg")
+    zoominWithoutRender()
+    img = Image.open("static/img/temp_img_zoomin.jpeg")
+    img = img.convert("RGB")
+
+    img_arr = np.asarray(img)
+
+    img_arr_shape = img_arr.shape
+    arr_x_size = img_arr.shape[0] * 4
+    arr_y_size = img_arr.shape[1] * 4
+
+    new_arr = np.full((arr_x_size, arr_y_size, 3), 255)
+    new_arr.setflags(write=1)
+
+    for row in range(img_arr_shape[0]):
+        for col in range(img_arr_shape[1]):
+            pix_1, pix_2, pix_3 = img_arr[row, col,
+                                          0], img_arr[row, col, 0], img_arr[row, col, 0]
+            new_arr[row, col, 0], new_arr[row + 1, col, 0], new_arr[row, col +
+                                                                    1, 0], new_arr[row + 1, col + 1, 0] = pix_1, pix_1, pix_1, pix_1
+            new_arr[row, col, 1], new_arr[row + 1, col, 1], new_arr[row, col +
+                                                                    1, 1], new_arr[row + 1, col + 1, 1] = pix_2, pix_2, pix_2, pix_2
+            new_arr[row, col, 2], new_arr[row + 1, col, 2], new_arr[row, col +
+                                                                    1, 2], new_arr[row + 1, col + 1, 2] = pix_3, pix_3, pix_3, pix_3
+            col += 1
+        row += 1
+
+    print(new_arr)
+    new_arr = np.uint8(new_arr)
+    img_new = Image.fromarray(new_arr)
+    img_new = img_new.convert("RGB")
+    img_new.save("static/img/temp_img_zoomout.jpeg")
+    return render_template("uploaded.html", file_path="img/temp_img_zoomout.jpeg")
+
+
+def zoominWithoutRender():
+    img = Image.open("static/img/temp_img.jpeg")
+    img = img.convert("RGB")
+
+    img_arr = np.asarray(img)
+    # img_arr.setflags(write=1)
+    new_size = ((img_arr.shape[0] // 2),
+                (img_arr.shape[1] // 2), img_arr.shape[2])
+    new_arr = np.full(new_size, 255)
+    print(img_arr.shape, new_size)
+    new_arr.setflags(write=1)
+
+    img_arr_shape = img_arr.shape
+
+    for row in range(img_arr_shape[0]):
+        for col in range(img_arr_shape[1]):
+            try:
+                new_arr[row, col, 0] = (int(img_arr[row, col, 0]) + int(img_arr[row + 1, col, 0]) + int(
+                    img_arr[row, col + 1, 0]) + int(img_arr[row + 1, col + 1, 0])) // 4
+                new_arr[row, col, 1] = (int(img_arr[row, col, 1]) + int(img_arr[row + 1, col, 1]) + int(
+                    img_arr[row, col + 1, 1]) + int(img_arr[row + 1, col + 1, 1])) // 4
+                new_arr[row, col, 2] = (int(img_arr[row, col, 2]) + int(img_arr[row + 1, col, 2]) + int(
+                    img_arr[row, col + 1, 2]) + int(img_arr[row + 1, col + 1, 2])) // 4
+            except:
+                break
+            col += 1
+        row += 1
+
+    new_arr = np.uint8(new_arr)
+    img_new = Image.fromarray(new_arr)
+    img_new.save("static/img/temp_img_zoomin.jpeg")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,3 +1,36 @@
+# import numpy as np
+# from PIL import Image
+
+# img = Image.open("static/img/temp_img.jpeg")
+# img = img.convert("RGB")
+
+# img_arr = np.asarray(img)
+# #img_arr.setflags(write=1)
+# new_size = ((img_arr.shape[0]//2), (img_arr.shape[1]//2), img_arr.shape[2])
+# new_arr = np.full(new_size, 255)
+# print(img_arr.shape, new_size)
+# new_arr.setflags(write=1)
+
+# img_arr_shape = img_arr.shape
+
+# for row in range(img_arr_shape[0]):
+#     for col in range(img_arr_shape[1]):
+#         try:
+#             new_arr[row,col,0] = (int(img_arr[row,col,0]) + int(img_arr[row+1,col,0]) + int(img_arr[row,col+1,0]) + int(img_arr[row+1,col+1,0])) // 4
+#             new_arr[row,col,1] = (int(img_arr[row,col,1]) + int(img_arr[row+1,col,1]) + int(img_arr[row,col+1,1]) + int(img_arr[row+1,col+1,1])) // 4
+#             new_arr[row,col,2] = (int(img_arr[row,col,2]) + int(img_arr[row+1,col,2]) + int(img_arr[row,col+1,2]) + int(img_arr[row+1,col+1,2])) // 4
+#         except:
+#             break
+#         col += 1
+#     row += 1
+
+# new_arr = np.uint8(new_arr)
+# img_new = Image.fromarray(new_arr)
+# #img_new = img_new.convert("RGB")
+# img_new.show()
+
+
+# Zoom Out
 import numpy as np
 from PIL import Image
 
@@ -5,26 +38,27 @@ img = Image.open("static/img/temp_img.jpeg")
 img = img.convert("RGB")
 
 img_arr = np.asarray(img)
-#img_arr.setflags(write=1)
-new_size = ((img_arr.shape[0]//2), (img_arr.shape[1]//2), img_arr.shape[2])
-new_arr = np.full(new_size, 255)
-print(img_arr.shape, new_size)
-new_arr.setflags(write=1)
 
 img_arr_shape = img_arr.shape
+arr_x_size = img_arr.shape[0] * 4
+arr_y_size = img_arr.shape[1] * 4
+
+new_arr = np.full((arr_x_size, arr_y_size, 3), 255)
+new_arr.setflags(write=1)
+
 
 for row in range(img_arr_shape[0]):
     for col in range(img_arr_shape[1]):
-        try:
-            new_arr[row,col,0] = (int(img_arr[row,col,0]) + int(img_arr[row+1,col,0]) + int(img_arr[row,col+1,0]) + int(img_arr[row+1,col+1,0])) // 4
-            new_arr[row,col,1] = (int(img_arr[row,col,1]) + int(img_arr[row+1,col,1]) + int(img_arr[row,col+1,1]) + int(img_arr[row+1,col+1,1])) // 4
-            new_arr[row,col,2] = (int(img_arr[row,col,2]) + int(img_arr[row+1,col,2]) + int(img_arr[row,col+1,2]) + int(img_arr[row+1,col+1,2])) // 4
-        except:
-            break
+        pix_1, pix_2, pix_3 = img_arr[row,col,0],img_arr[row,col,0],img_arr[row,col,0]
+        new_arr[row, col, 0], new_arr[row+1, col, 0], new_arr[row, col+1, 0], new_arr[row+1, col+1, 0] = pix_1, pix_1,pix_1,pix_1
+        new_arr[row, col, 1], new_arr[row+1, col, 1], new_arr[row, col+1, 1], new_arr[row+1, col+1, 1] = pix_2, pix_2,pix_2,pix_2
+        new_arr[row, col, 2], new_arr[row+1, col, 2], new_arr[row, col+1, 2], new_arr[row+1, col+1, 2] = pix_3, pix_3,pix_3,pix_3
         col += 1
     row += 1
 
+print(new_arr)
 new_arr = np.uint8(new_arr)
 img_new = Image.fromarray(new_arr)
-#img_new = img_new.convert("RGB")
+img_new = img_new.convert("RGB")
 img_new.show()
+
