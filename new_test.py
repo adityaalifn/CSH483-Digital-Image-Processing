cc3e60819802e3036121d34350597a94e1d157f5
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-02-25 01:49:15 +0700

    percobaan

--- new_test.py
+++ new_test.py
@@ -10,9 +10,9 @@ flipped_arr.setflags(write=1)
 
 for row in range(img_arr.shape[0]):
     for col in range(img_arr.shape[1]):
-        flipped_arr[row, -1*(col+1), 0] = img_arr[row,col,0]
-        flipped_arr[row, -1*(col+1), 1] = img_arr[row,col,1]
-        flipped_arr[row, -1*(col+1), 2] = img_arr[row,col,2]
+        flipped_arr[-1*(row+1), col, 0] = img_arr[row,col,0]
+        flipped_arr[-1*(row+1), col, 1] = img_arr[row,col,1]
+        flipped_arr[-1*(row+1), col, 2] = img_arr[row,col,2]
         
 img_new = Image.fromarray(flipped_arr)
 img_new = img_new.convert("RGB")
